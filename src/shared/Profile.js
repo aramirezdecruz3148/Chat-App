7a9a0ac085a87de810bf52bde26bd31953817efd
aramirezdecruz3148 <leigh3148@gmail.com>
2019-06-03 15:57:04 -0700

    successfully DDD-ed my Profile component for all three instances user, no photp, and no user

--- src/shared/Profile.js
+++ src/shared/Profile.js
@@ -3,8 +3,19 @@ import Component from '../Component.js';
 class Profile extends Component {
 
     renderTemplate() {
+        const user = this.props.user;
+        if(!user) {
+            return `<div></div>`;
+        }
+
+        const avatar = user.photoURL || './assets/default-avatar.png';
+
         return /*html*/`
-            
+        <div class="profile">
+            <img src="${avatar}">
+            <span>${user.displayName}</span>
+            <button>Sign Out</button>
+        </div>
         `;
     }
 }
