bdc26b2be1f3929969dce8350661854d3ce742f9
aramirezdecruz3148 <leigh3148@gmail.com>
2019-06-03 15:01:28 -0700

    set up initial file structure

---
+++ src/services/firebase.js
@@ -0,0 +1,13 @@
+const config = {
+    apiKey: 'AIzaSyBbBfsW1s_3MFVf0PxgyebsDLfOp5aH6zg',
+    authDomain: 'bootcamp2-bac7d.firebaseapp.com',
+    databaseURL: 'https://bootcamp2-bac7d.firebaseio.com',
+    projectId: 'bootcamp2-bac7d',
+    storageBucket: 'bootcamp2-bac7d.appspot.com',
+    messagingSenderId: '856095394433',
+    appId: '1:856095394433:web:a0a43c492022b440'
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
