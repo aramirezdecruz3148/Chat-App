abae397c3f9af206207f32b556f9bba0b8a7e09c
aramirezdecruz3148 <leigh3148@gmail.com>
2019-06-03 15:28:18 -0700

    I hooked up my header element to my App and got it to render

---
+++ src/shared/Header.js
@@ -0,0 +1,14 @@
+import Component from '../Component.js';
+
+class Header extends Component {
+
+    renderTemplate() {
+        return /*html*/`
+            <header>
+                <h1>Chat App</h1>
+            </header>
+        `;
+    }
+}
+
+export default Header;
