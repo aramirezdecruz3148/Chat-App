0dda60000d139007df25be173d8481c33357a201
aramirezdecruz3148 <leigh3148@gmail.com>
2019-06-05 08:51:02 -0700

    I added some mock chat data to get everything connected and rendered before encorporating firebase

---
+++ mockData.js
@@ -0,0 +1,16 @@
+const chats = [
+    {
+        chat: 'Puppies Chat Room'
+    },
+    {
+        chat: 'Kitties Chat Room'
+    },
+    {
+        chat: 'Harry Potter Chat Room'
+    },
+    {
+        chat: 'Embriodery Chat Room'
+    }
+];
+
+export default chats;
