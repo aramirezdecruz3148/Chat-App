c70a0f16b4a1303ce4b7253b922b9b33ead91c85
aramirezdecruz3148 <leigh3148@gmail.com>
2019-06-05 08:16:49 -0700

    styled AddChat component

--- src/home/AddChat.js
+++ src/home/AddChat.js
@@ -3,7 +3,7 @@ import Component from '../Component.js';
 class AddChat extends Component {
     renderTemplate() {
         return /*html*/`
-            <form>
+            <form id="chat-form">
                 <label>Add a Chat Room to List:<input name="chat"></label>
                 <button>Add</button>
             </form>
