720e6e664aa95223861c64c47a09f12c4021c294
aramirezdecruz3148 <leigh3148@gmail.com>
2019-06-06 08:27:02 -0700

    rendered my header onto my ChatApp

--- src/chat/ChatApp.js
+++ src/chat/ChatApp.js
@@ -1,6 +1,16 @@
 import Component from '../Component.js';
+import Header from '../shared/Header.js';
 
 class ChatApp extends Component {
+    render() {
+        const dom = this.renderDOM();
+        const main = dom.querySelector('main');
+        const header = new Header({ title: 'chat Chat CHAT AWAY!' });
+        dom.insertBefore(header.render(), main);
+
+        return dom;
+    }
+
     renderTemplate() {
         return /*html*/`
             <div>
