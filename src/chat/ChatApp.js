64ccb4db554316f19299888fe49d5cb1d2cca0ca
aramirezdecruz3148 <leigh3148@gmail.com>
2019-06-06 07:33:25 -0700

    created chat.html, ChatApp and chat.js, hooked them all up and rendered it so that my chatroom buttons go to the page

---
+++ src/chat/ChatApp.js
@@ -0,0 +1,13 @@
+import Component from '../Component.js';
+
+class ChatApp extends Component {
+    renderTemplate() {
+        return /*html*/`
+            <div>
+                <main>
+                </main>
+            </div>
+        `;
+    }
+}
+export default ChatApp;
