f08e917d0cbaf864c79dcc3eb1758ff3ffb5eb43
aramirezdecruz3148 <leigh3148@gmail.com>
2019-06-06 15:31:36 -0700

    created an event listener and checked with console.log that it works on click

--- src/chat/MakeMessage.js
+++ src/chat/MakeMessage.js
@@ -1,6 +1,17 @@
 import Component from '../Component.js';
 
 class MakeMessage extends Component {
+    render() {
+        const form = this.renderDOM();
+
+        form.addEventListener('submit', event => {
+            event.preventDefault();
+            
+        });
+
+        return form;
+    }
+
     renderTemplate() {
         return /*html*/`
         <form id="make-message">
