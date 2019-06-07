bd17b23506342fc5ceb023eb5b23681959aa9b7c
aramirezdecruz3148 <leigh3148@gmail.com>
2019-06-07 07:37:14 -0700

    interpolated all the data from firebase and organized it onto my chat page

--- src/chat/MessageItem.js
+++ src/chat/MessageItem.js
@@ -4,7 +4,14 @@ class MessageItem extends Component {
     renderTemplate() {
         const message = this.props.message;
         return /*html*/`
-            <li>${message.message}</li>
+            <li>
+                <span>
+                    <img src="${message.photoURL}">
+                    <h4>${message.displayName}</h4>
+                    <h4>${message.message}</h4>
+                    <p>${message.date}</p>
+                </span>
+            </li>
         `;
     }
 }
